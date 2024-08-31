6f7b444cb4488dbf91d83c3c21241163d2061e75
Carlos García Rosales <carlosgarcia_97gr@hotmail.com>
2024-08-31 20:59:49 +0200

    add error check create folder

--- folders/folders.go
+++ folders/folders.go
@@ -13,29 +13,85 @@ func NewFolders() *Folders {
 }
 
 // Create a folder if it does not exist
-func (f *Folders) CreateFolder(route string) {
-	os.Mkdir(route, 0755)
+func (f *Folders) CreateFolder(route string) error {
+	if err := os.Mkdir(route, 0755); err != nil {
+		if os.IsExist(err) {
+			fmt.Println("Folder already exists: ", route)
+			return nil
+		} else {
+			return err
+		}
+	}
+	return nil
 }
 
-func (f *Folders) CreateFolderStructure(projectName string, subAppName []string) {
-	f.CreateFolder(projectName + "/internal")
-	f.CreateFolder(projectName + "/cmd")
-	f.CreateFolder(projectName + "/cmd/http")
-	f.CreateFolder(projectName + "/configs")
-	f.CreateFolder(projectName + "/scripts")
-	f.CreateFolder(projectName + "/pkg")
-	f.CreateFolder(projectName + "/infrastructure")
-	f.CreateFolder(projectName + "/infrastructure/docker")
+func (f *Folders) CreateFolderStructure(projectName string, subAppName []string) error {
+	var err error
+	err = f.CreateFolder(projectName + "/internal")
+	if err != nil {
+		return err
+	}
+	err = f.CreateFolder(projectName + "/cmd")
+	if err != nil {
+		return err
+	}
+	err = f.CreateFolder(projectName + "/cmd/http")
+	if err != nil {
+		return err
+	}
+	err = f.CreateFolder(projectName + "/configs")
+	if err != nil {
+		return err
+	}
+	err = f.CreateFolder(projectName + "/scripts")
+	if err != nil {
+		return err
+	}
+	err = f.CreateFolder(projectName + "/pkg")
+	if err != nil {
+		return err
+	}
+	err = f.CreateFolder(projectName + "/infrastructure")
+	if err != nil {
+		return err
+	}
+	err = f.CreateFolder(projectName + "/infrastructure/docker")
+	if err != nil {
+		return err
 
+	}
 	for _, route := range subAppName {
-		f.CreateFolder(projectName + "/internal/" + route)
-		f.CreateFolder(projectName + "/internal/" + route + "/domain")
-		f.CreateFolder(projectName + "/internal/" + route + "/domain/ports")
-		f.CreateFolder(projectName + "/internal/" + route + "/application")
-		f.CreateFolder(projectName + "/internal/" + route + "/infrastructure/repository")
-		f.CreateFolder(projectName + "/internal/" + route + "/infrastructure/handler")
-		f.CreateFolder(route)
+		var err error
+		err = f.CreateFolder(projectName + "/internal/" + route)
+		if err != nil {
+			return err
+		}
+		err = f.CreateFolder(projectName + "/internal/" + route + "/domain")
+		if err != nil {
+			return err
+		}
+		err = f.CreateFolder(projectName + "/internal/" + route + "/domain/ports")
+		if err != nil {
+			return err
+		}
+		err = f.CreateFolder(projectName + "/internal/" + route + "/application")
+		if err != nil {
+			return err
+		}
+		err = f.CreateFolder(projectName + "/internal/" + route + "/infrastructure/repository")
+		if err != nil {
+			return err
+		}
+		err = f.CreateFolder(projectName + "/internal/" + route + "/infrastructure/handler")
+		if err != nil {
+			return err
+		}
+		err = f.CreateFolder(route)
+		if err != nil {
+			return err
+		}
 	}
 
 	fmt.Println("Folder structure created successfully!")
+	return nil
 }
